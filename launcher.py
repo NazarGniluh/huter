8b5c1a4cd12ce98f55bc1023bb2c477e42780f3c
Anonymous <ostap.sukhomlyn@gmail.com>
2024-02-04 17:01:26 +0200

    buy

--- launcher.py
+++ launcher.py
@@ -27,9 +27,14 @@ print(settings)
 start = QPushButton("ПОЧАТИ")
 change = QPushButton("Change")
 skin_1 = QLabel("КАРТИНКА")
+skin_2 = QLabel("ПОкупка")
+skin_2_img = QPixmap("bullet.png")
+skin_2_img = skin_2_img.scaledToWidth(64)
+skin_2.setPixmap(skin_2_img)
 skin_1_img = QPixmap("asteroid.png")
 skin_1_img = skin_1_img.scaledToWidth(64)
 skin_1.setPixmap(skin_1_img)
+skin_2_buy = QPushButton("КУПити айфон")
 skin_1_buy = QPushButton("КУПИТИ СКІН")
 line_edit = QLineEdit(settings["skin"])
 linuz = QHBoxLayout()
@@ -38,7 +43,9 @@ linuz.addWidget(start)
 linuz.addWidget(line_edit)
 linuz.addWidget(change)
 linuz.addWidget(skin_1)
+linuz.addWidget(skin_2)
 linuz.addWidget(skin_1_buy)
+linuz.addWidget(skin_2_buy)
 window.setLayout(linuz)
 
 def buy_skin_1():
@@ -48,10 +55,19 @@ def buy_skin_1():
         write_data()
     else:
         print(" НЕ ХВАТАЄ ГРОШЕЙ!!")
+
+def buy_skin_2():
+    if settings["money"] >= 7:
+        settings["money"] -= 7
+        settings["skin"] = "bullet.png"
+        write_data()
+    else:
+        print(" НЕ ХВАТАЄ ГРОШЕЙ!!")
 def change_data():
     settings["skin"] = line_edit.text()
     write_data()
 
+skin_2_buy.clicked.connect(buy_skin_2)
 skin_1_buy.clicked.connect(buy_skin_1)
 change.clicked.connect(change_data)
 start.clicked.connect(start_game)
